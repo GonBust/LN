23200ba33f6a2cf378101e19435a51a565984c0b
Guilherme Gomes <jguilhermefgomes@gmail.com>
2018-10-13 23:02:26 +0100

    mmm2mm

--- Mini_proj1/run.sh
+++ Mini_proj1/run.sh
@@ -6,3 +6,7 @@ rm -f *.fst
 #Compila e gera versao grafica do transdutor en-pt
 fstcompile --isymbols=palavras.syms --osymbols=palavras.syms en2pt.txt | fstarcsort > en2pt.fst
 fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait en2pt.fst | dot -Tpdf  > en2pt.pdf
+
+######## MMM para MM #########
+fstcompile --isymbols=palavras.syms --osymbols=palavras.syms mmm2mm.txt | fstarcsort > mmm2mm.fst
+fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
