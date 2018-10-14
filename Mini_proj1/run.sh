e1ca9937cebd4962128a4831a4430ce824649028
gonbust <goncalo.bustorff.silva@tecnico.ulisboa.pt>
2018-10-14 01:55:05 +0100

    a) and b) done

--- Mini_proj1/run.sh
+++ Mini_proj1/run.sh
@@ -2,15 +2,22 @@
 
 rm -f *.fst
 
-########## Tradutor Ingles para Portugues ##########
-#Compila e gera versao grafica do transdutor en-pt
-fstcompile --isymbols=palavras.syms --osymbols=palavras.syms en2pt.txt | fstarcsort > en2pt.fst
-fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait en2pt.fst | dot -Tpdf  > en2pt.pdf
-
 ######## MMM para MM #########
+echo "Criado transdutor de MMM para MM"
 fstcompile --isymbols=palavras.syms --osymbols=palavras.syms mmm2mm.txt | fstarcsort > mmm2mm.fst
 fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
 
 ######## DD/MMM/AAAA para DD/MM/AAAA #########
+echo "Criado transdutor de DD/MMM/AAAA para DD/MM/AAAA"
 fstcompile --isymbols=palavras.syms --osymbols=palavras.syms misto2numerico.txt | fstarcsort > misto2numerico.fst
 fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait misto2numerico.fst | dot -Tpdf  > misto2numerico.pdf
+
+########## Tradutor Ingles para Portugues ##########
+echo "Criado transdutor de en para pt"
+fstcompile --isymbols=palavras.syms --osymbols=palavras.syms en2pt.txt | fstarcsort > en2pt.fst
+fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait en2pt.fst | dot -Tpdf  > en2pt.pdf
+
+########## Tradutor Portuges para Ingles ##########
+echo "Criado transdutor de pt para en"
+fstcompile --isymbols=palavras.syms --osymbols=palavras.syms pt2en.txt | fstarcsort > pt2en.fst
+fstdraw    --isymbols=palavras.syms --osymbols=palavras.syms --portrait pt2en.fst | dot -Tpdf  > pt2en.pdf
