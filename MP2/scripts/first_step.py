b1f9f1801f50a08563109230dc1d65d846f2b0f4
gonbust <goncalo.bustorff.silva@tecnico.ulisboa.pt>
2018-10-28 16:49:39 +0000

    skeleton done

--- MP2/scripts/first_step.py
+++ MP2/scripts/first_step.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, re, codecs
 import nltk
+from nltk.metrics.scores import precision, recall
 
 #---------------
 # Faz o print de uma lista
@@ -11,12 +12,26 @@ def print_list(list):
 		print (list[j])
 		j = j + 1
 
+#---------------
+# Lê a ref
+#----------------
+def read_ref(file):
+	ref = []
+	for line in file:
+		ref.append(line.strip())
+	print_list(ref)
+	print('\n')
+	return set(ref)
+
+#---------------
+# Lê as Questoes e associa tag
+#----------------
 def InOut(file):
-	numLinha = 0
 	myset = []
 	for line in file:
 		myset.append('actor_name')
 	print_list(myset)
+	print('\n')
 	return set(myset)
 
 #--------------
@@ -25,3 +40,14 @@ def InOut(file):
 fileIn = open('../Corpora/NovasQuestoes.txt', 'r')
 myset = InOut(fileIn)
 fileIn.close()
+
+#--------------
+# Extrai REF
+#--------------
+fileRef = open('../Corpora/NovasQuestoesResultados.txt', 'r')
+ref = read_ref(fileRef)
+fileRef.close()
+
+
+print ("Precision:", precision(ref, myset))
+print ("Recall:", recall(ref, myset))
