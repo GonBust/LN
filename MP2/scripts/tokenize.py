3afebdc7e1f724c0cb4b94e818f672dc25d36de7
mrpatinhazz <jguilhermefgomes@gmail.com>
2018-11-02 01:47:09 +0000

    Tests and tuple beggining
    Also a small todo.

---
+++ MP2/scripts/tokenize.py
@@ -0,0 +1,26 @@
+import nltk, re, pprint
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB
+
+tagSet = {'actor_name','budget','character_name','genre','keyword','original_language',
+'original_title','overview','person_name', 'production_company', 'production_country',
+'release_date', 'revenue', 'runtime','spoken_language', 'vote_avg'}
+
+fNewQs = open('corpora\\NovasQuestoes.txt').read()
+newQstk = nltk.sent_tokenize(fNewQs)
+
+with open('corpora\\QuestoesConhecidas.txt') as f:
+    _knqslist = f.readlines()
+_knqslist = [x.strip() for x in _knqslist]
+knTags = []
+knQs = []
+
+for x in _knqslist:
+    tag, q = x.split(" \t")
+    knTags.append(tag)
+    knQs.append(q)
+
+#to do - each Q' bag of words. each Q as document? (movie reviews)
+#      - training set
+#      - remember this needs to be multiclass
