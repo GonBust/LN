2cb43ed407da102c1fcd7dba6ce8d1399015656a
gonbust <goncalo.bustorff.silva@tecnico.ulisboa.pt>
2018-11-04 15:40:50 +0000

    decesion tree no stemming

--- MP2/scripts/tokenize_MP2.py
+++ MP2/scripts/tokenize_MP2.py
@@ -20,12 +20,6 @@ def remove_stop_words(sent):
     new_sent = "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in filtered]).strip()
     return new_sent
 
-def stem_words(sent):
-    word_tokens = word_tokenize(sent)
-    stemmed = [ps.stem(w) for w in word_tokens]
-    new_sent = "".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in stemmed]).strip()
-    return new_sent
-
 #Lê todas as linhas do ficheiro "QuestoesConhecidas" e guarda-as numas lista. Em seguida separa os elementos por \n (linha)
 with open(known_q) as f:
     _knqslist = f.readlines()
@@ -37,8 +31,7 @@ train_data = []
 for x in _knqslist:
     tag, q = x.split(' \t')
     q = remove_stop_words(re.sub(' \n','',q))
-    q_stemmed = stem_words(q)
-    train_data.append((q_stemmed,tag))
+    train_data.append((q,tag))
 
 print('\ntrain data:\n')
 for i in range(0, 10):
@@ -50,17 +43,16 @@ all_words = set(word.lower() for passage in train_data for word in word_tokenize
 t = [({word : (word in word_tokenize(x[0])) for word in all_words}, x[1]) for x in train_data]
 
 #o classificador NaiveBayes é treinado com o set
-classifier = nltk.NaiveBayesClassifier.train(t)
+classifier = nltk.DecisionTreeClassifier.train(t)
 
 #Guarda todas as questões com as stopwords removidas. Tokeniza as frases resultantes.
 fNewQs = open(new_q).read()
 newQstk = nltk.sent_tokenize(fNewQs)
 newQstk_no_stop_words = [remove_stop_words(sent) for sent in newQstk]
-newQstk_stemmed = [stem_words(sent) for sent in newQstk_no_stop_words]
 
-print('\nnewQstk_stemmed:\n')
+print('\nnewQstk no stop words:\n')
 for i in range(0, 10):
-    print(f'{newQstk_stemmed[i]}')
+    print(f'{newQstk_no_stop_words[i]}')
 
 #Guarda e tokeniza os resultados
 fNewQsResult = open(new_q_res).read()
@@ -68,7 +60,7 @@ newQstkRes = nltk.word_tokenize(fNewQsResult)
 
 #Usando o classificador, classifica as novas questões
 results = []
-for x in newQstk_stemmed:
+for x in newQstk_no_stop_words:
     x_features = {word.lower(): (word in word_tokenize(x.lower())) for word in all_words}
     results.append(classifier.classify(x_features))
 
@@ -78,7 +70,7 @@ for i in range(0, 42):
     line += 1
     print(f'{line}\t{newQstkRes[i]}\t\t{results[i]}')
 
-print('\nWrong results:\nline: Question:\t\t\t\t\t\tright result:\t\tresult:')
+print('\nWrong results:\nline: Question:\t\t\t\t\t\t\t\tright result:\t\tresult:')
 line = 0
 for i in range(0, 42):
     line += 1
